c2057f8bfa991b1e9b63e130c45142b6100dffaf
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-03-13 08:50:15 -0300

    solucionar error de trim

--- src/Components/Main/Formularios/NoControlados/NoControlados.jsx
+++ src/Components/Main/Formularios/NoControlados/NoControlados.jsx
@@ -1,7 +1,70 @@
+import { useRef, useState } from "react";
+
+
 
 export const NoControlados = () =>{
 
-    return 'No controlados'
+    const form = useRef(null);
+    const [error, setError] = useState("")
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        setError("");
+        
+        // capturar datos
+        const data = new FormData(form.current)
+        const { title, description, state } = Object.fromEntries([
+            ...data.entries()
+        ])
+
+        // Validar datos
+        if(!title.trim() || !description.trim() || !state.trim())
+            return setError('Llena todos los campos')
+
+
+        //Enviar datos
+        console.log( title, description, state )
+    }
+
+    // document.addEventListener('submit', (event)=>{
+    //     event.preventDefault()
+    // })
+
+    return (
+        <>
+            <h2>Formularios No Controlados</h2>
+            <form onSubmit={handleSubmit} ref={form}>
+                <input
+                    type="text"
+                    placeholder="Ingrese Todo"
+                    className="form-control mb-2"
+                    name="title"
+                    defaultValue="todo #01"
+                />
+
+                <textarea
+                    className="form-control mb-2"
+                    placeholder="Ingrese dirección"
+                    name="descripcion"
+                    defaultValue="descripción #01"
+                ></textarea>
+
+                <select
+                    className="form-select mb-2"
+                    name="state"
+                    defaultValue="completado"
+                >
+                    <option value="pendiente">Pendiente</option>
+                    <option value="completado">Completado</option>
+                </select>
+
+                <button type="submit" className="btn btn-primary">
+                    Procesar
+                </button>
+                {error !== '' && error}
+            </form>
+        </>
+    );
 
 
 }
